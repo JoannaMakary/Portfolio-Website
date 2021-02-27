81868ffef95ccc0ac2ed97478cd0524d0bee573e
Jo <joannamakary@hotmail.com>
2021-02-26 22:37:35 -0500

    New style of main page

--- scripts/filter.js
+++ scripts/filter.js
@@ -11,6 +11,7 @@
 $(document).ready(function () {
   // Shows all projects
   $("#filter-reset").click(function () {
+    $("#world-of-league").show();
     $("#prc-caterers").show();
     $("#prc-caterers2").show();
     $("#jo-nutritional").show();
@@ -24,8 +25,25 @@ $(document).ready(function () {
     $("#rgb-game").show();
   });
 
+  // Shows only projects with PHP
+  $("#filter-php").click(function () {
+    $("#world-of-league").show();
+    $("#prc-caterers").hide();
+    $("#prc-caterers2").hide();
+    $("#jo-nutritional").hide();
+    $("#maple-royals").hide();
+    $("#max-brakes").hide();
+    $("#leaguelity").hide();
+    $("#yelp-camp").hide();
+    $("#boop-bot").hide();
+    $("#art-store").hide();
+    $("#backbone").hide();
+    $("#rgb-game").hide();
+  });
+
   // Shows only projects with Javascript
   $("#filter-js").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").show();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
@@ -41,6 +59,7 @@ $(document).ready(function () {
 
   // Shows only projects with Node.js
   $("#filter-node").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
@@ -56,6 +75,7 @@ $(document).ready(function () {
 
   // Shows only projects with Python
   $("#filter-python").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").show();
@@ -71,6 +91,7 @@ $(document).ready(function () {
 
   // Shows only projects with Express
   $("#filter-express").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
@@ -86,6 +107,7 @@ $(document).ready(function () {
 
   // Shows only projects with Angular
   $("#filter-angular").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
@@ -101,6 +123,7 @@ $(document).ready(function () {
 
   // Shows only projects with React
   $("#filter-react").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
@@ -116,6 +139,7 @@ $(document).ready(function () {
 
   // Shows only projects with MongoDB
   $("#filter-mongo").click(function () {
+    $("#world-of-league").hide();
     $("#prc-caterers").hide();
     $("#prc-caterers2").hide();
     $("#jo-nutritional").hide();
